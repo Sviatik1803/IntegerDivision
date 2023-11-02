a0dfe55baa4de4121a5f1c306b163241b6e8de92
Sviatoslav Shistka <svyatoslav182003@gmail.com>
2023-11-02 11:50:10 +0200

    fix: add exception and resolve sonarlint issues

--- src/main/java/ua/sviatik/formatters/FormatterFactory.java
+++ src/main/java/ua/sviatik/formatters/FormatterFactory.java
@@ -1,14 +1,18 @@
 package ua.sviatik.formatters;
 
-
 import ua.sviatik.entity.DividingType;
 import ua.sviatik.formatters.impl.ClassicFormatter;
 
-public class FormatterFactory {
+public final class FormatterFactory {
     public static Formatter getFormatter(String format) {
+
         if (format.equalsIgnoreCase(DividingType.CLASSIC.name())) {
             return new ClassicFormatter();
         }
-        throw new RuntimeException("!!!");
+        throw new IllegalArgumentException("Incorrect argument of type");
+    }
+
+    private FormatterFactory() {
+
     }
 }
